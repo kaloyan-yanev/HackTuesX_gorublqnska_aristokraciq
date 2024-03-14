d98fcfeb4ba65ac6d8d4856ac35d15c70d10cfd5
ViktorAlexiev <viktor.i.aleksiev.2022@elsys-bg.org>
2024-03-14 19:19:46 +0200

    testing relays and temperature sensors

--- akvarium/akvarium.ino
+++ akvarium/akvarium.ino
@@ -1,139 +1,49 @@
-#include <ESP32Servo.h>
-#include <RTClib.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define lampPin 14
-#define pumpPin 27
-#define servoPin 26
-#define reostatPin 33
-#define termoAkvarium 12
-#define termoVunshno 13
-#define flowPin 25
-#define buzzerPin 4
-#define modeChangePin 15
-#define pumpManagePin 2
+#define SON_IN 32
+#define SON_OUT 22
+#define lampPin 2
+#define pumpPin 3
+#define servoPin 4
+#define reostatPin 5
 
-Servo myServo;
-OneWire temp1(termoAkvarium);
-OneWire temp2(termoVunshno);
-DallasTemperature termoSonda1(&temp1);
-DallasTemperature termoSonda2(&temp2);
-RTC_DS1307 rtc;
+OneWire son_in(SON_IN);
+OneWire son_out(SON_OUT);
 
-int servoPos = 90;
-unsigned long timeServoOpen = 3000;
-unsigned long timePumping = 1000;
-unsigned long timeBetweenPumps = 5000;
-bool isPumping = false;
-bool isChangingWater = false;
-int waterTemp = 0;
-int wantedWaterTemp = 25;
-int modeButtonState;
-bool currentlyFeeding = false;
+DallasTemperature sonIn(&son_in);
+DallasTemperature sonOut(&son_out);
 
-//============================================================================================
+void setup(void)
+{
+    Serial.begin(115200);
 
-void Feeding(){
-  bool doneFeeding = false;
-  unsigned long timeNow = 0;
-  int period = timeServoOpen;
-  servoPos = 180;
-  myServo.write(servoPos);
-  if(doneFeeding == false){
-    if(millis() >= timeNow + period){
-      servoPos = 90;
-      myServo.write(servoPos);
-      doneFeeding = true;
-    }
-  }
-}
+    pinMode(lampPin, OUTPUT);
+    pinMode(pumpPin, OUTPUT);
+    pinMode(servoPin, OUTPUT);
+    pinMode(reostatPin, OUTPUT);
 
-void Pumping(){
-  bool donePumping = false;
-  unsigned long timeNow = 0;
-  int period = timePumping;
-  isPumping = true;
-  if(donePumping == false){
-    if(millis() >= timeNow + period){
-       isPumping = false;
-       donePumping = true;
-    }
-  }
-}
-
-void LightsOn(){
-  digitalWrite(lampPin, HIGH);
-}
-
-void LightsOff(){
-  digitalWrite(lampPin, LOW);
-}
-
-void Zagrqvane(){
-   if(waterTemp < wantedWaterTemp){
-    digitalWrite(reostatPin, HIGH);
-   }else{
-    digitalWrite(reostatPin, LOW);
-   }
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Charging the capacitors. Wait 10 seconds");
-  delay(10000);
-  
-  myServo.attach(servoPin);
-  myServo.write(servoPos);
-  pinMode(pumpPin, OUTPUT);
-  pinMode(modeChangePin, INPUT);
-  pinMode(pumpManagePin, INPUT);
-  if (! rtc.begin()) {
-    Serial.println("RTC module is not found");
-    while (1);
-  }
-
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  
-  termoSonda1.begin();
-  termoSonda2.begin();
-}
-
-void loop() {
-  DateTime currentTime = rtc.now();
-  waterTemp = termoSonda1.getTempCByIndex(0);
-  modeButtonState = digitalRead(modeChangePin);
-  if(modeButtonState == HIGH){
-    isChangingWater = !isChangingWater; 
-    digitalWrite(pumpPin, LOW);
-  }
-  if(isChangingWater == true){
-    int newWaterTemp = termoSonda2.getTempCByIndex(0);
-    int pumpMangageState = digitalRead(pumpManagePin);
-    if(newWaterTemp < wantedWaterTemp){
-      Serial.println("Temperature of the new water is colder");
-    }
-
+    sonIn.begin();
+    sonOut.begin();
     
-    if(pumpManagePin == HIGH){
-      if(pumpPin == LOW){
-        digitalWrite(pumpPin, HIGH);
-      }else{
-        digitalWrite(pumpPin, LOW);
-      }
-    }
-  }
-  if(isChangingWater == false){
-    unsigned long timePumpDelay = 0;
-    int periodPumpDelay = timeBetweenPumps;
-    Zagrqvane();
+}
 
-    if(millis() >= timePumpDelay + periodPumpDelay){
-      if(isPumping == true){
-        digitalWrite(pumpPin, HIGH);
-      }else{
-        digitalWrite(pumpPin, LOW);
-      }
+void loop(void)
+{
+    sonIn.requestTemperatures();
+    sonOut.requestTemperatures();
+    Serial.println(sonIn.getTempCByIndex(0));
+    Serial.println(sonOut.getTempCByIndex(0));
+
+    if(sonIn.getTempCByIndex(0) > 27){
+      digitalWrite(lampPin, HIGH);
+      digitalWrite(pumpPin, HIGH);
+      digitalWrite(servoPin, HIGH);
+      digitalWrite(reostatPin, HIGH);
+    }else{
+      digitalWrite(lampPin, LOW);
+      digitalWrite(pumpPin, LOW);
+      digitalWrite(servoPin, LOW);
+      digitalWrite(reostatPin, LOW);
     }
-  }
 }
